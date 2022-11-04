0c540defca38f4ff5c3cc70e8860c3750e2f3912
TusharBhavsar18 <tusharbhavsar2011@gmail.com>
2022-11-04 08:49:21 +0530

    code is fixed

--- prime.js
+++ prime.js
@@ -1,10 +1,24 @@
+//Identify_Prime...//
+
 
 function checkprime(num){
- for(let i=2;i<=num;i++){
-if(num%i==0){
-count++;
-} 
-} 
-if(count==1){
-console.log("prime");
+
+    let factor = 0;
+    for(let i=1 ; i<=num ; i++)
+    {
+       if(num%i==0)
+       {
+          factor++;
+       }
+    }
+       if(count==2)
+       {
+          console.log("Yes");
+       }
+       else
+       {
+          console.log("No");
+       }
 }
+
+checkprime(13)
